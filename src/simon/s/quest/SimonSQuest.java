12dd3b9548be4cfd33d0c9fdf0b97ba95cd26617
WX <rlaterson@gmail.com>
2017-06-24 09:26:00 -0400

    Stuff

--- src/simon/s/quest/SimonSQuest.java
+++ src/simon/s/quest/SimonSQuest.java
@@ -5,17 +5,51 @@
  */
 package simon.s.quest;
 
+import com.opengg.core.engine.GGApplication;
+import com.opengg.core.engine.OpenGG;
+import com.opengg.core.engine.RenderEngine;
+import com.opengg.core.engine.Resource;
+import com.opengg.core.render.shader.ShaderController;
+import com.opengg.core.render.texture.Cubemap;
+import com.opengg.core.render.window.WindowInfo;
+import com.opengg.core.render.window.WindowOptions;
+import static com.opengg.core.render.window.WindowOptions.GLFW;
+import com.opengg.core.world.Skybox;
+
 /**
  *
  * @author Warren
  */
-public class SimonSQuest {
+public class SimonSQuest extends GGApplication{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        WindowInfo w = new WindowInfo();
+        w.name = "Simon, Simon Quest Simon's Simon Quest Simon Simon Quest Quest";
+        w.displaymode = WindowOptions.WINDOWED;
+        w.width = 1920;
+        w.height = 1080;
+        w.resizable = true;
+        w.type = GLFW;
+        w.vsync = true;
+        OpenGG.initialize(new SimonSQuest(), w);
+    }
+
+    @Override
+    public void setup() {
+         RenderEngine.setSkybox(new Skybox(Cubemap.get(Resource.getTexturePath("skybox\\majestic")), 1500f)); 
+    }
+
+    @Override
+    public void render() {
+        ShaderController.setPerspective(90, OpenGG.window.getRatio(), 0.2f, 3000f);
+    }
+
+    @Override
+    public void update() {
+        
     }
     
 }
