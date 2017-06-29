820336fbe4b751af5d2f34808110e0e0e1f26ff7
WX <rlaterson@gmail.com>
2017-06-28 22:31:49 -0400

    Stats

--- Simon's Quest/src/simon/s/quest/Player.java
+++ Simon's Quest/src/simon/s/quest/Player.java
@@ -12,7 +12,9 @@ import simon.s.quest.inventory.Inventory;
  * @author Warren
  */
 public class Player {
-
+    
+    public static String name;
+    
     public static Inventory inv = new Inventory();
 
     public static int health;
@@ -20,5 +22,15 @@ public class Player {
     public static double cash;
 
     public static int level;
+    public static int exptonextlevel;
+    public static int exp;
     
+    public static int soulurge;
+    public static int destinynumber;
+    
+    public static int attack;
+    public static int defense;
+    
+    public static int mattack;
+    public static int mdefense;
 }
